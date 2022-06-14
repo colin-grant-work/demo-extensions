b611d603a2cef92d077203de2045952e8fa3cf0c
Colin Grant <colin.grant@ericsson.com>
2022-06-14 15:26:31 -0600

    Add tabbar submenu

--- menu-contribution-test/src/template.js
+++ menu-contribution-test/src/template.js
@@ -25,7 +25,8 @@ for (const point of supportedContributionPoints) {
 console.log(JSON.stringify({
     activationEvents: supportedContributionPoints.map(point => `onCommand:${toCommandId(point)}`).concat('onCommand:menu-contribution-test.thumbsDown', 'onCommand:menu-contribution-test.thumbsUp'),
     contributes: {
-        commands: supportedContributionPoints.map(point => ({ command: toCommandId(point), title: `Appears in: ${point}`, icon: '$(beaker)' })).concat({ command: 'menu-contribution-test.thumbsDown', title: 'Make the thumb point down', icon: '$(thumbsup)' }, { command: 'menu-contribution-test.thumbsUp', title: 'Make the thumb point up', icon: '$(thumbsdown)' }),
+        commands: supportedContributionPoints.map(point => ({ command: toCommandId(point), title: `Appears in: ${point}`, icon: '$(beaker)' })).
+            concat({ command: 'menu-contribution-test.thumbsDown', title: 'Make the thumb point down', icon: '$(thumbsup)' }, { command: 'menu-contribution-test.thumbsUp', title: 'Make the thumb point up', icon: '$(thumbsdown)' }),
         menus: supportedContributionPoints.reduce((accumulated, current) => {
             accumulated[current] = [{ command: toCommandId(current) }];
             if (current === 'explorer/context' || current === 'scm/title') {
@@ -33,6 +34,9 @@ console.log(JSON.stringify({
             }
             if (current === 'view/title') {
                 accumulated[current].push({ command: 'menu-contribution-test.thumbsDown', when: 'menu-contribution-test.contextKey != thumbsDown', group: 'navigation' }, { command: 'menu-contribution-test.thumbsUp', when: 'menu-contribution-test.contextKey == thumbsDown', group: 'navigation' });
+            }
+            if (current === 'editor/title') {
+                accumulated[current].push({ submenu: 'menu-contribution-test.submenu', when: 'resourceExtname == .ts', group: 'navigation' });
             }
             return accumulated;
         }, Object.create(null)),
